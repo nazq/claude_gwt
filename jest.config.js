693d6fb852872280f9c3c19ac3b97c1626728c78
Naz Quadri <naz.quadri@gmail.com>
2025-07-04 17:20:54 -0400

    fix: Set coverage thresholds to 20% temporarily
    Will increase coverage thresholds later as we add more tests

--- jest.config.js
+++ jest.config.js
@@ -18,8 +18,8 @@ module.exports = {
   ],
   coverageThreshold: {
     global: {
-      branches: 15,
-      functions: 15,
+      branches: 20,
+      functions: 20,
       lines: 20,
       statements: 20,
     },
